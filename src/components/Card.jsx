d89ff7ae1b034b5335627c023587aaf7494824d1
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2025-11-07 14:22:14 +0100

    Card flipping done with react package

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import ReactCardFlip from 'react-card-flip';
+
+const Card = ({ id, isFlipped, onClick, image }) => {
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      {/* Front of the card (shown when not flipped) */}
+      <div 
+        className="frontCard"
+        onClick={() => onClick(id)}
+      />
+
+      {/* Back of the card (shown when flipped) */}
+      <div 
+        className="backCard"
+        onClick={() => onClick(id)}
+      >
+        <img 
+          src={image} 
+          alt="card"
+        />
+      </div>
+    </ReactCardFlip>
+  );
+};
+
+export default Card;
+
+
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,31 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
 
-function GameBoard() {
-  return (
+// Import all card images
+import butterfly from '../assets/cardImages/butterfly.png';
+import dove from '../assets/cardImages/dove.png';
+import dragon from '../assets/cardImages/dragon.png';
+import flamingo from '../assets/cardImages/flamingo.png';
+import ladybug from '../assets/cardImages/ladybug.png';
+import peacock from '../assets/cardImages/peacock.png';
+import penguin from '../assets/cardImages/penguin.png';
+import phoenix from '../assets/cardImages/phoenix.png';
+import toucan from '../assets/cardImages/toucan.png';
+import unicorn from '../assets/cardImages/unicorn.png';
+
+const GameBoard = () => {
+  const cardImages = [
+    butterfly,
+    dove,
+    dragon,
+    flamingo,
+    ladybug,
+    peacock,
+    penguin,
+    phoenix,
+    toucan,
+    unicorn
+  ];
+
+  // Create pairs of cards
+  const createBoard = () => {
+    const pairs = [...cardImages, ...cardImages];
+    return pairs.map((image, index) => ({
+      id: index,
+      image: image,
+      isFlipped: false,
+      isMatched: false
+    })).sort(() => Math.random() - 0.5);
+  };
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(createBoard());
+  }, []);
+
+  const handleCardClick = (cardId) => {
+  
+    const newCards = cards.map(card => {
+      if (card.id === cardId) {
+        return { ...card, isFlipped: true };
+      }
+      return card;
+    });
+
+    setCards(newCards);
+    setFlippedCards([...flippedCards, newCards.find(card => card.id === cardId)]);
+  
+  };
+
+  return ( 
     <div className='Cards'>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        <div className='Card'></div>
-        
+      {cards.map(card => (
+        <Card
+          key={card.id}
+          id={card.id}
+          isFlipped={card.isFlipped}
+          image={card.image}
+          onClick={handleCardClick}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default GameBoard;
